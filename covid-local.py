632142a5e6c908a79f704be1738d64585680d3f3
Chris Smowton <chris@smowton.net>
2020-08-03 14:05:43 +0100

    Just enough style to be readable

--- covid-local.py
+++ covid-local.py
@@ -96,15 +96,16 @@ def write_index(regions):
 
   for ordering in ("index", "peak", "recent_cases", "recent_rel_peak"):
     with open(os.path.join(outdir, ordering + ".html"), "w") as f:
-      f.write("<html><body><h1>Covid cases in England by region</h1>\n")
+      f.write("<html><head><style>td { padding: 3; }\n.data { text-align: center; }</style></head>\n")
+      f.write("<body><h1>Covid cases in England by region</h1>\n")
       f.write('<p><a href="https://coronavirus.data.gov.uk/">Source data</a> -- <a href="https://github.com/smowton/covid-local">source code</a></p>\n')
       for (type, regions) in sorted(regions_by_type.iteritems(), key = lambda kv: len(kv[1])):
         f.write("<h2>Region type: %s</h2>\n<table>\n" % type)
-        f.write('<tr><td><a href="index.html">Name</a></td><td><a href="peak.html">Peak date</a></td><td><a href="recent_cases.html">Cases last fortnight</a></td><td><a href="recent_rel_peak.html">Cases last fortnight, prop. of peak</a></td></tr>\n')
+        f.write('<tr><td><a href="index.html">Name</a></td><td class="data"><a href="peak.html">Peak date</a></td><td class="data"><a href="recent_cases.html">Cases last fortnight</a></td><td class="data"><a href="recent_rel_peak.html">Cases last fortnight, prop. of peak</a></td></tr>\n')
         sort_order = None if ordering is "index" else lambda regname: regions_stats[(regname, type)][ordering]
         for region in sorted(regions, key = sort_order, reverse = ordering != "index"):
           stats = regions_stats[(region, type)]
-          f.write('<tr><td><a href="%s">%s</a></td><td>%s</td><td>%d</td><td>%.1f%%</td></tr>\n' % \
+          f.write('<tr><td><a href="%s">%s</a></td><td class="data">%s</td><td class="data">%d</td><td class="data">%.1f%%</td></tr>\n' % \
              (escape(region + " " + type) + ".png", \
               region, \
               datetime.datetime.strftime(stats["peak"], '%Y-%m-%d'), \
