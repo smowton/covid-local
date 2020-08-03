344f038cffdd2a404a339418a238a80d72213eb4
Chris Smowton <chris@smowton.net>
2020-08-03 13:44:47 +0100

    Add stats

--- covid-local.py
+++ covid-local.py
@@ -54,11 +54,14 @@ def pad_series(recs):
     out.append(rec)
   return out
 
-def write_graph(region_name, region_type, records):
-  print "Graph", region_name, region_type
+def clean_and_add_averages(records):
   records = sorted(records, key = lambda rec: rec[1])
   records = pad_series(records)
   records = [(date_str, date, cases, seven_day_avg(i, records)) for (i, (date_str, date, cases)) in enumerate(records)]
+  return records
+
+def write_graph(region_name, region_type, records):
+  print "Graph", region_name, region_type
   first_date = min(rec[1] for rec in records)
   fname = os.path.join(outdir, escape(region_name + " " + region_type))
   fig, ax = plt.subplots(figsize=(12, 8), dpi=100)
@@ -71,6 +74,16 @@ def write_graph(region_name, region_type, records):
   fig.savefig(fname)
   plt.close()
 
+def region_stats(region_name, region_type):
+  recs = regions[(region_name, region_type)]
+  recent_cases = sum(rec[2] for rec in recs[-14:])
+  peak_rec = max(recs, key = lambda rec: rec[3])
+  return {
+    "peak": peak_rec[1],
+    "recent_cases": recent_cases,
+    "recent_rel_peak": recent_cases / float(14 * peak_rec[3])
+  }
+
 def write_index(regions):
   print "Write index"
   regions_by_type = dict()
@@ -79,14 +92,28 @@ def write_index(regions):
       regions_by_type[region_type] = []
     regions_by_type[region_type].append(region_name)
 
-  with open(os.path.join(outdir, "index.html"), "w") as f:
-    f.write("<html><body><h1>Covid cases in England by region</h1>\n")
-    for (type, regions) in sorted(regions_by_type.iteritems(), key = lambda kv: len(kv[1])):
-      f.write("<h2>Region type: %s</h2>\n<ul>\n" % type)
-      for region in sorted(regions):
-        f.write('<li><a href="%s">%s</a></li>\n' % (escape(region + " " + type) + ".png", region))
-      f.write("</ul>\n<hr/>\n")
-    f.write("</body></html>")
+  regions_stats = {region: region_stats(*region) for region in regions}
+
+  for ordering in ("index", "peak", "recent_cases", "recent_rel_peak"):
+    with open(os.path.join(outdir, ordering + ".html"), "w") as f:
+      f.write("<html><body><h1>Covid cases in England by region</h1>\n")
+      for (type, regions) in sorted(regions_by_type.iteritems(), key = lambda kv: len(kv[1])):
+        f.write("<h2>Region type: %s</h2>\n<table>\n" % type)
+        f.write('<tr><td><a href="index.html">Name</a></td><td><a href="peak.html">Peak date</a></td><td><a href="recent_cases.html">Cases last fortnight</a></td><td><a href="recent_rel_peak.html">Cases last fortnight, prop. of peak</a></td></tr>\n')
+        sort_order = None if ordering is "index" else lambda regname: regions_stats[(regname, type)][ordering]
+        for region in sorted(regions, key = sort_order, reverse = ordering != "index"):
+          stats = regions_stats[(region, type)]
+          f.write('<tr><td><a href="%s">%s</a></td><td>%s</td><td>%d</td><td>%.1f%%</td></tr>\n' % \
+             (escape(region + " " + type) + ".png", \
+              region, \
+              datetime.datetime.strftime(stats["peak"], '%Y-%m-%d'), \
+              stats["recent_cases"], \
+              stats["recent_rel_peak"] * 100))
+        f.write("</table>\n<hr/>\n")
+      f.write("</body></html>")
+
+for key, records in regions.iteritems():
+  regions[key] = clean_and_add_averages(regions[key])
 
 for (region_name, region_type), records in regions.iteritems():
   write_graph(region_name, region_type, records)
